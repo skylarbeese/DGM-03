26ef81421aa2c429911f918e212bb71f4435806b
SKYLAR BEESE <skylarbeese@gmail.com>
2021-02-04 17:29:44 -0700

    con

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+let correctNum = Math.floor(Math.random() * 15)
+
+let guessed = false
+let totalGuesses = 0
+let gamerGuess = 0
+correctNum += 1
+
+console.log(`this is the number ${correctNum}`)
+
+function elv() {
+  
+  totalGuesses += 1
+  gamerGuess = document.querySelector('guess')
+  const feedBack = document.querySelector('feedback')
+  console.log(totalGuesses, gamerGuess)
+ /*if(gamerGuesses == correctNum) {
+     console.log('the number is correct')
+ } else {
+    console.log('the number is not correct')
+ } */
+}
